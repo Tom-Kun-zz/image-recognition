ce1618dc6a86ce3b410cf80f4df5f2237fef99b7
Tom-Kun <thomas.bazzucchi@gmail.com>
2019-07-17 18:26:22 +0200

    Adding the three different example

--- FirstPrediction.py
+++ FirstPrediction.py
@@ -6,7 +6,17 @@ prediction.setModelTypeAsResNet()
 prediction.setModelPath( execution_path + "/resnet50_weights_tf_dim_ordering_tf_kernels.h5")
 prediction.loadModel()
 
+print("\nFirst Image should be a Tesla Model")
+predictions, percentage_probabilities = prediction.predictImage(execution_path + "/examples/sample1.jpg", result_count=5)
+for index in range(len(predictions)):
+  print(predictions[index] , " : " , percentage_probabilities[index])
 
+print("\nSecond Image should be a Lamborghini Model")
 predictions, percentage_probabilities = prediction.predictImage(execution_path + "/examples/sample2.jpg", result_count=5)
+for index in range(len(predictions)):
+  print(predictions[index] , " : " , percentage_probabilities[index])
+
+print("\nThird Image should be a Dacia Duster (SUV)")
+predictions, percentage_probabilities = prediction.predictImage(execution_path + "/examples/sample3.jpg", result_count=5)
 for index in range(len(predictions)):
   print(predictions[index] , " : " , percentage_probabilities[index])
